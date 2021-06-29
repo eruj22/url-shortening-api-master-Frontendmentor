6261c6be1810558a8dedd5de759a6ded9ade73f5
eruj22 <prci.prnaver2@gmail.com>
2021-06-29 20:50:11 +0200

    finishing responsive layout

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+// mobile navigation
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.nav__hamburger');
 
 navBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
+
+// shortening url
+const getUrl = document.querySelector('.url');
+const getUrlBtn = document.querySelector('.cta-shorten');
+
+const params = {
+    'url': 'https://www.frontendmentor.io/solutions'
+};
+const options = {
+    method: 'POST',
+    body: JSON.stringify( params )
+};
+fetch( 'https://api.shrtco.de/v2/', options )
+    .then( response => response.json() )
+    .then( response => {
+        console.log(response);
+    } );
